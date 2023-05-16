3544c66074838506458f60e2a10502427db62be1
Sebastian Medina <snmedina91@gmail.com>
2023-05-16 14:17:45 -0300

    add images and navbar effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,59 @@
-import Link from 'next/link'
-import React from 'react'
-import Logo from './Logo'
+'use client'
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import {  usePathname } from "next/navigation";
 
-const CustomLink = ({href, title, className=''}) => {
-    return(
-        <Link href={href} className={`${className}`}>
-            {title}
-        </Link>
-    )
-}
+const CustomLink = ({ href, title, className = "" }) => {
+  
+  const pathname = usePathname()
+  
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span className={`h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 
+      group-hover:w-full transition-[width] ease duration-300 ${pathname === href ? 'w-full' : 'w-0'}`}>
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const Navbar = () => {
   return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
-        <nav>
-            <CustomLink href='/' title="home" className='mr-4'/>
-            <CustomLink href='/about' title='about' className='mr-4'/>
-            <CustomLink href='/projects' title='Projects' className='mr-4'/>
-            <CustomLink href='/articles' title='Articles' className='mr-4'/>
-        </nav>
-        
-        <nav>
-            <CustomLink href='/' target={"_blank"}>targetas</CustomLink>
-            <CustomLink href='/' target={"_blank"}>T</CustomLink>
-            <CustomLink href='/' target={"_blank"}>T</CustomLink>
-            <CustomLink href='/' target={"_blank"}>T</CustomLink>
-            <CustomLink href='/' target={"_blank"}>T</CustomLink>
-            <CustomLink href='/' target={"_blank"}>T</CustomLink>
-        </nav>
-        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
-        <Logo />
-        </div>
-    </header>
-  )
-}
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <CustomLink href="/" title="home" className="mr-4" />
+        <CustomLink href="/about" title="about" className="mr-4" />
+        <CustomLink href="/projects" title="Projects" className="mr-4" />
+        <CustomLink href="/articles" title="Articles" className="mr-4" />
+      </nav>
 
-export default Navbar
+      <nav>
+        <CustomLink href="/" target={"_blank"}>
+          targetas
+        </CustomLink>
+        <CustomLink href="/" target={"_blank"}>
+          T
+        </CustomLink>
+        <CustomLink href="/" target={"_blank"}>
+          T
+        </CustomLink>
+        <CustomLink href="/" target={"_blank"}>
+          T
+        </CustomLink>
+        <CustomLink href="/" target={"_blank"}>
+          T
+        </CustomLink>
+        <CustomLink href="/" target={"_blank"}>
+          T
+        </CustomLink>
+      </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
