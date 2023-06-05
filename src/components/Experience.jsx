31e484844ef210774a7ded6eb92aba21fcd18020
Sebastian Medina <snmedina91@gmail.com>
2023-06-05 09:10:02 -0300

    experiences

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -34,7 +34,7 @@ const Experience = () => {
   return (
     <div className="my-64">
       <h2 className="font-bold text-8xl mb-32 w-full text-center">
-        Experience
+        Experiences
       </h2>
 
       <div ref={ref} className="w-[75%] mx-auto relative">
