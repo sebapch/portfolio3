1b5d8884a9fa1fe164d7042317318570e6730dc8
Sebastian Medina <snmedina91@gmail.com>
2023-06-14 17:15:01 -0300

    addd Projects page

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,74 @@
+import AnimatedText from "@/components/AnimatedText";
+import { GithubIcon } from "@/components/Icons";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import project1 from "../../../public/images/projects/crypto-screener-cover-image.jpg";
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full flex items-center justify-between
+    rounded-3xl border border-solid border-dark bg-light shadow-2xl
+    ">
+      <Link href={link} target="_blank">
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div>
+        <span>{type}</span>
+        <Link href={link} target="_blank">
+          <h2>{title}</h2>
+        </Link>
+        <p>{summary}</p>
+        <div>
+          <Link href={github} target="_blank">
+            <GithubIcon />
+          </Link>
+          <Link href={link} target="_blank">
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const page = () => {
+  return (
+    <>
+      <Head>
+        <title>Projects</title>
+        <meta name="description" content="fullstack dev" />
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Imagination Trumps Knowledge!" className="py-2" />
+
+          <div className="grid grid-cols-12 gap-24">
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Crypto Screener Application"
+                img={project1}
+                summary=" A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+                    It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                    local currency."
+                link="/"
+                github=""
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">Project 1</div>
+            <div className="col-span-6">Project 2</div>
+            <div className="col-span-12">Featured project</div>
+            <div className="col-span-6">Project 3</div>
+            <div className="col-span-6">Project 4</div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default page;
