803eba0b83509c49714e6438558333d893f8e2b8
Sebastian Medina <snmedina91@gmail.com>
2023-07-25 12:37:51 -0300

    fix

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -29,7 +29,7 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
             <GithubIcon />
           </Link> 
           <Link href={link} target="_blank" className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold">
-            Visit this Projects
+            Visit this Project
           </Link>
         </div>
       </div>
