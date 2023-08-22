5f7f4a38d38d202fe38a4971eb8803ad12f68e85
Sebastian Medina <snmedina91@gmail.com>
2023-08-21 21:16:20 -0300

    main responsive

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -42,7 +42,7 @@ const page = () => {
       </Head>
       <main className="flex w-full flex-col items-center justify-center dark:text-light ">
         <Layout className="pt-16">
-          <AnimatedText text="Passion Fuels Purpose!" className="mb-16"/>
+          <AnimatedText text="Passion Fuels Purpose!" className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"/>
           <div className="grid w-full grid-cols-8 gap-16 ">
             <div className="col-span-3 flex flex-col items-start justify-start">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
